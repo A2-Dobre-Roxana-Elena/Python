fd830ca00e8062e08d55a9068be8b7017adf6de8
A2-Dobre-Roxana-Elena <79217476+A2-Dobre-Roxana-Elena@users.noreply.github.com>
2022-10-10 09:27:57 +0300

    exercitii complete terminate acasa

--- seminar1.py
+++ seminar1.py
@@ -1,3 +1,16 @@
+
+#exercitiul 1
+"""
+import math
+
+x = list(map(int, input("Introduceti valorile: ").split()))
+gcd=x[0]
+for i in x:
+    gcd=math.gcd(gcd,i)
+
+print ("Cel mai mare divizor comun al numerelor introduse este : ", gcd) 
+"""
+
 # exercitiul 2
 """
 print("Introduceti un sir de caractere ")
@@ -32,9 +45,29 @@ text_de_prelucrat=text_de_prelucrat.replace(' ','_')
 print("Textul prelucrat acum este ", text_de_prelucrat)
 """
 
+#exercitiul 5
+"""
+import string
+
+squareMatrix =[ ['f','i','r','s'],['n','_','l','t'] ,['o','b','a','_'],['h','t','y','p'] ]
+stringObtain=''
+for i in range(4):
+    stringObtain+= squareMatrix[0][i]
+for i in range(1,4):
+    stringObtain+= squareMatrix[i][3]
+for i in range(3,0,-1):
+    stringObtain+= squareMatrix[i][0]
+for i in range(1,3):
+    stringObtain+= squareMatrix[1][i]
+for i in range(2,0,-1):
+    stringObtain+= squareMatrix[2][i]
+print(stringObtain)
+"""
+
+
 #exercitiul 6
 """
-def validate(numar):
+def isPalindrome(numar):
     numar=str(numar)
     
     x= numar[len(numar)::-1]
@@ -47,10 +80,10 @@ def validate(numar):
 
 print("Introduceti un numar")
 numar=input()
-print(validate(numar))"""
+print(isPalindrome(numar))"""
 
 #exercitiul 7 
-    """
+"""
 def eCifra(x):
     nr_cifre=0
     nr_cifre+=x.count('0') + x.count('1') + x.count('2') + x.count('3') + x.count('4')
@@ -60,18 +93,73 @@ def eCifra(x):
     else:
         return True
 
+def retNumar(fraza):
+    
+    numar=0
+    primul=False
+    for x in fraza:
+        if eCifra(x) :
+            numar=numar*10+int(x)
+            if primul==False:
+                primul=True
+        else:
+            if primul==True:
+                break    
+    return numar
+
 print("Introduceti un sir de caractere ")
 fraza=input()
-numar=0
-for x in fraza:
-    print(x)
-    while eCifra(x):
+print(retNumar(fraza))"""
+
+#exercitiul 8
+"""
+from itertools import count
+
+def bitiUnu(inputNum):
+    inputNum=int(inputNum)
+    inputNum=bin(inputNum)
+    numarString=str(inputNum)
+    nrBitiUnu=numarString.count('1')
+    return nrBitiUnu
+
+print("Introduceti un numar:")
+numar=input()
+print("Numarul de biti cu valuarea 1 este egal cu ", bitiUnu(numar))
+"""
+
+#exercitiul 9
+"""
+def caracterComun(sir):
+    nr=0
+    caracter='7'
+    for i in  range(ord('a'), ord('z')+1): 
+        if(nr<sir.count(chr(i))):
+            nr=sir.count(chr(i))
+            caracter=chr(i)
+    for i in  range(ord('A'), ord('Z')+1): 
+        if(nr<sir.count(chr(i))):
+            nr=sir.count(chr(i))
+            caracter=chr(i)
+    return caracter
 
 
-    if eCifra(x) :
-        print("Da",x)
-    else:
-        print("Nu",x)
-    """
+print("Introduceti un sir de caractere:")
+sirCaractere=input()
+print("Caracterul cel mai des intalnit in sirul dat este:",caracterComun(sirCaractere))
+"""
+
 
 #exercitiul 10 
+"""
+def numarCuvinte(fraza):
+    nrCuvinte=1
+    for x in fraza:
+        if x==' ':
+            nrCuvinte+=1
+    return nrCuvinte
+
+print("Introduceti un sir de caractere ")
+fraza=input()
+print("In textul dat s au gasit ",numarCuvinte(fraza)) 
+"""
+
